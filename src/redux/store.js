c60481d7089d3d8bc39de34ef289ee6ee80b0e2f
Joshua Duenow <joshjduenow@gmail.com>
2023-12-01 08:48:07 -0600

    cleaning up code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,50 +1,46 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
 // Be sure to replace this reducer! 🙂
 const pizzaMenu = (state = [], action) => {
-  if (action.type === 'SET_PIZZA') {
-    return action.payload
-}
+  if (action.type === "SET_PIZZA") {
+    return action.payload;
+  }
   return state;
-}
+};
 
 const cart = (state = [], action) => {
-  if (action.type === 'ADD_PIZZA_TO_CART') {
-    return [...state, action.payload]
-}
-if (action.type === 'REMOVE_PIZZA_FROM_CART') {
-  return state.filter((pizza) => {
-    return pizza.id != action.payload.id
-  })
-}
-if (action.type === 'CLEAR_CART') {
-  return []
-}
+  if (action.type === "ADD_PIZZA_TO_CART") {
+    return [...state, action.payload];
+  }
+  if (action.type === "REMOVE_PIZZA_FROM_CART") {
+    return state.filter((pizza) => {
+      return pizza.id != action.payload.id;
+    });
+  }
+  if (action.type === "CLEAR_CART") {
+    return [];
+  }
   return state;
-}
+};
 
 const customerInfo = (state = [], action) => {
-  if (action.type === 'CREATE_CUSTOMER_INFO') {
-    return action.payload
-}
-if (action.type === 'CLEAR_CART') {
-  return []
-}
+  if (action.type === "CREATE_CUSTOMER_INFO") {
+    return action.payload;
+  }
+  if (action.type === "CLEAR_CART") {
+    return [];
+  }
   return state;
-}
-
-
-
+};
 
 const store = createStore(
   combineReducers({
     pizzaMenu,
     cart,
-    customerInfo
+    customerInfo,
   }),
-  applyMiddleware(logger),
+  applyMiddleware(logger)
 );
-
 
 export default store;
